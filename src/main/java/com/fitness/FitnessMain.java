d933cfaee66a2623dbebe0184c3292fbe932a33f
Kaal581993 <viral.prajapati.1993@gmail.com>
2026-01-22 22:05:29 +0530

    Updated entities

--- src/main/java/com/fitness/FitnessMain.java
+++ src/main/java/com/fitness/FitnessMain.java
@@ -1,11 +1,11 @@
+package com.fitness;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class FitnessMain {
-
-
     public static void main(String[] args) {
         SpringApplication.run(FitnessMain.class, args);
     }
---
+++ src/main/java/com/fitness/model/ActivityType.java
@@ -0,0 +1,24 @@
+package com.fitness.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    SWIMMING,
+    BOXING,
+    PILATES,
+    STRENGTH_TRAINING,
+    SLEEPING,
+    JUMPING,
+    MARTIAL_ARTS,
+    AEROBICS,
+    CRICKET,
+    FOOTBALL,
+    TENNIS,
+    HOCKEY,
+    BATMINTON,
+    CYCLING,
+    OUTDOOR_CYCLING,
+    YOGA,
+    HIIT,
+    CARDIO_TRAINING
+}
